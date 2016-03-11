fd5d289a56f188caf787339b04a0003508069258
CarltonSemple <csemp@outlook.com>
2016-03-11 01:00:47 -0500

    RGB to Util class

--- sass/headers/util.h
+++ sass/headers/util.h
@@ -9,10 +9,16 @@ class Util
     public:
     static double distance(std::pair<double,double> p1, std::pair<double,double> p2)
     {
-        double m = pow(p1.first - p2.first, 2) + pow(p2.second - p2.second, 2);
+        double m = pow(p1.first - p2.first, 2) + pow(p1.second - p2.second, 2);
         double s = sqrt(m);
         return s;
     }  
+};
+
+struct RGB {
+    uchar r;
+    uchar g;
+    uchar b;  
 };
 
 #endif
