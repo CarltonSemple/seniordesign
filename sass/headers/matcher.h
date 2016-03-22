e5a5744b9a1a3d1a0d7d779add77fe53099c6760
CarltonSemple <csemp@outlook.com>
2016-03-22 13:58:57 -0400

    Fixed compilation with matcher.h

--- sass/headers/matcher.h
+++ sass/headers/matcher.h
@@ -1,9 +1,14 @@
 #ifndef MATCHER_H
 #define MATCHER_H
 
+#include <iostream>
 #include <vector>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
+#include <opencv2/features2d/features2d.hpp>
+#include <opencv2/calib3d/calib3d.hpp>
+#include "human.h"
+#include "robustMatcher.h"
 
 class Matcher 
 {
