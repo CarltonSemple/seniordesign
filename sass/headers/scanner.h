e865161d910c49a91f24a888e66eef9d484b66c4
CarltonSemple <csemp@outlook.com>
2016-03-24 11:32:01 -0400

    Added function to display a video of the specified scan

--- sass/headers/scanner.h
+++ sass/headers/scanner.h
@@ -1,7 +1,12 @@
 #ifndef SCANNER_H
 #define SCANNER_H
 
+#include <iostream>
+#include <vector>
 #include "human.h"
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
 
 class Scanner 
 {
@@ -22,6 +27,20 @@ private:
 public:
     void runIndependently();
     Human & loadScannedHuman(int setNum);
+    
+    static void displayScan(int scanNumber)
+    {
+        Scanner s;
+        Human scannedHuman = s.loadScannedHuman(2);
+        std::cout << "loaded " << scannedHuman.getImageCount() << " pictures" << std::endl;
+            
+        std::vector<cv::Mat> & selfies = scannedHuman.getImages();
+        for(int i = 0; i < selfies.size(); i++)
+        {
+            cv::imshow("video capture", selfies[i]);//img);
+            cv::waitKey(10);
+        }
+    }
 };
 
 #endif
