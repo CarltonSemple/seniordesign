cb6d31f4b38e03d2a939c84ead1e741530eada04
Justin Villani <juvillan@syr.edu>
2016-04-12 13:27:31 -0400

    Update Commites and Formatting to tcp_server.c
    I added a bunch of commits as well as made sure that the code was formatted all the same. Making sure the code is well documented for further read back of the code.

--- parrot/packages/Samples/Unix/JSPilotingNewAPI/tcp_server.c
+++ parrot/packages/Samples/Unix/JSPilotingNewAPI/tcp_server.c
@@ -8,8 +8,10 @@
 #include <netdb.h> 
 #include <arpa/inet.h>
 
+/*Can be changed, just value we used*/
 #define BUF_SIZE 1024
 
+/*Function that captures errors from error checking in code*/
 void error(const char *msg)
 {
     perror(msg);
@@ -18,19 +20,19 @@ void error(const char *msg)
 
 int main(int argc, char *argv[])
 {
-	/*Server File and port number being used*/
+    /*Server File and port number being used*/
     int parentfd, portno;
     /*Two Child processes for both unqiue Drones*/
     int drone_1, drone_2;
-	/*Byte Size of Client Addr*/
-	int clientlen;
+    /*Byte Size of Client Addr*/
+    int clientlen;
     /*Server and Client Address Setup*/
     struct sockaddr_in serv_addr;
     struct sockaddr_in client_addr_drone_1;
-	struct sockaddr_in client_addr_drone_2;
+    struct sockaddr_in client_addr_drone_2;
     /*Client Host Info*/
     struct hostent *hostp;
-	/*This is the buffer holding our data being sent*/
+    /*This is the buffer holding our data being sent*/
     char buffer[BUF_SIZE];
 	/*Dotted decimal Host address String*/
 	char *hostaddrp;
@@ -48,7 +50,7 @@ int main(int argc, char *argv[])
 
     /*Create server socket*/
     parentfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	/*Checking Socket Created*/
+	/*Checking Socket Creation*/
 	if (parentfd < 0)
 	{	 
 		error("ERROR opening socket");
@@ -68,15 +70,13 @@ int main(int argc, char *argv[])
 		error("ERROR on binding");
 	}
 	 
-	/*Listen: make this socket ready to accpet connection requests*/
+	/*Listen: make this socket ready to accept connection requests*/
 	if(listen(parentfd,2) < 0)
 	{
 		error("ERROR on listen");
 	}
-	
+	/*Getting the length of the client message*/
 	clientlen = sizeof(client_addr_drone_1);
-	
-	/*Main Loop: wait for a connection request*/
 	
 	/*************Setting up the Connections for Both Drones***********************/
 		/*Accepting Connection from Drone 1*/
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 		{
 			error("ERROR on accept Drone 1");
 		}
-		printf("Drone 1 accept\n");
+		printf("Drone 1 accepted\n");
 		/*Accepting Connection from Drone 2*/
 		drone_2 = accept(parentfd, (struct sockaddr*) &client_addr_drone_2, &clientlen);
 		if(drone_2 < 0)
@@ -93,9 +93,8 @@ int main(int argc, char *argv[])
 			error("ERROR on accept Drone 1");
 		}
 		/***************************************************************************/
-
-		printf("Both Drones connected to System\n");
-
+		printf("Drone 2 accepted\n");
+		printf("\nBoth Drones connected to System\n");
 		/****************Syncing the Drones sockets with server socket**************/
 		/*Reading info from Drone 1*/		
 		bzero(buffer,BUF_SIZE);
@@ -114,7 +113,7 @@ int main(int argc, char *argv[])
 			error("ERROR reading from Drone 2");
 		}
 		/**************************/
-
+		/*Just putting a NULL value in buffer so there is something in there*/
 		buffer[0] = ' ';
 		/* Sending sync info to Drone 1*/
 		n_1=write(drone_1, buffer, strlen(buffer));
@@ -131,32 +130,36 @@ int main(int argc, char *argv[])
 			error("ERROR writing to Drone 2");	
 		}
 		/**************************/
-
-		/**********************************************************************/
+	/**********************************************************************/
+	
+	/*Main Loop: wait for a connection request*/
 	// Send to server
 	while (buffer[0] != 'q') 
 	{	
 
 		if (buffer[0] != 'q') 
 		{
+			/*Setting the values in buffer to zero*/
 			bzero(buffer, BUF_SIZE);
 
-			// Get desired drone to pilot
+			/*Get desired drone to pilot*/
 			printf("Enter which drone you wish to pilot (1 or 2, or 3 to Quit): ");
 			scanf("%i", &desired_drone);
+			/*If 3 is received we are going to quit the connection to both drones*/
 			if (desired_drone == 3)
 			{
 				printf("Setting quit_flag to 1\n");
 				quit_flag = 1;
 			}
+			/*If no values are choose as shown above you will default and control drone 1*/
 			else if (desired_drone != 1 && desired_drone != 2)
 			{
 				desired_drone = 1;
 			}
-
+			/*If quit_flag is not triggered, then we proceed in sending instruction to desired drone*/
 			if (!quit_flag)
 			{
-				// Get desired drone movement
+				/*Get desired drone movement/instruction*/
 				printf("Enter input for drone movement\n(A: Left, W: Forward, S: Backward, D: Right)\n");
 				scanf("%s", buffer);
 				if (buffer[0] == 'a' || buffer[0] == 'd')
@@ -167,13 +170,12 @@ int main(int argc, char *argv[])
 				{
 					printf("Enter number of inches to move\n");
 				}
+				/*This is scanning in how far you want to go from which instruction you chose from WASD*/
 				scanf("%i", &desired_length);
-			
-				// Move drone by sending buffer desired_length times
+				/*Move drone by sending buffer desired_length times*/
 				for (int i=0; i<desired_length; i++)
 				{
 					printf("Sending %s to drone %i", buffer, desired_drone);
-
 					if (desired_drone == 1)
 					{
 						n_1=write(drone_1, buffer, strlen(buffer));
@@ -193,8 +195,10 @@ int main(int argc, char *argv[])
 						printf("...Sent\n");
 					}
 				}
-			
-				// Stop moving drone
+				/*Stop moving drone*/
+				/*We are sending x as the ending value to determine when to stop moving drone*/
+				/*Example: Choose w 3, which really is sending w,w,w,x. So we are sending 3 forward movements followed by the ending
+				value x which will stop sending "w" and the drones will wait for next instruction*/
 				buffer[0] = 'x';
 				printf("Sending %s to drone %i", buffer, desired_drone);
 				if (desired_drone == 1)
@@ -218,7 +222,7 @@ int main(int argc, char *argv[])
 			}
 			else
 			{
-				// Send quit to drone
+				/*Sending quit to drone both drones and cut client connection to server*/
 				buffer[0] = 'q';
 				printf("Sending %s to drone %i", buffer, desired_drone);
 				n_1=write(drone_1, buffer, strlen(buffer));
@@ -236,7 +240,8 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-		
+	
+	/*Closing the connection from the server as well as the two clients when you quit*/	
     close(parentfd);
 	close(drone_1);
 	close(drone_2);
