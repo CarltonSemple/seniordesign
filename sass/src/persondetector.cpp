4ed02ef24eb98df40aff51288d2ce0641a331a64
Carlton Semple <CarltonSemple@users.noreply.github.com>
2016-04-12 23:19:45 -0400

    HOG speed increase
    DetectHumans now makes a scaled down copy of the input image, runs HOG on it, and then draws scaled up rectangles over the original input image.

--- sass/src/persondetector.cpp
+++ sass/src/persondetector.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include "opencv2/highgui.hpp"
+#include <opencv2/core/utility.hpp>
+#include <opencv2/core/ocl.hpp>
 #include "persondetector.h"
  
 using namespace std;
@@ -9,26 +12,65 @@ PersonDetector::PersonDetector()
     
 }
 
-std::vector<Human> PersonDetector::detectHumans(Mat sourceImg, bool drawBoxes) 
+std::vector<Human> PersonDetector::detectHumans(cv::UMat sourceImg, bool drawBoxes) 
 {
     vector<Human> humans;
     
     HOGDescriptor hog;
     hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+       
+    UMat scaledDown;
+    Size sSize(320, 240);
+    resize(sourceImg, scaledDown, sSize);
+    
+    double scale = sourceImg.size().height / scaledDown.size().height;
+    //cout << "scale: " << scale << endl;
+   
+    int win_width = 24;//48;
+    int win_stride_width = 8;
+    int win_stride_height = 8;
+    int gr_threshold = 8;
+    int nlevels = 13;
+    double hit_threshold = 1.4;
+    double scaleHog = 1.05;
+    bool gamma_corr = true;
+    Size win_size(win_width, win_width * 2);
+    Size win_stride(win_stride_width, win_stride_height);
+    //HOGDescriptor hog(win_size, Size(16, 16), Size(8, 8), Size(8, 8), 9, 1, -1, HOGDescriptor::L2Hys, 0.2, gamma_corr, cv::HOGDescriptor::DEFAULT_NLEVELS);
+ 
+    /*
+    if  ( ! cv::ocl::haveOpenCL ())
+    {
+        cout  <<  "OpenCL IS not avaiable ..."  <<  endl ;
+            //return 0;
+    } else {
+        cout << "openCL is available :) " << endl;
+    }*/
+    
+    
+    //cv::ocl::attachContext();
+    // gpu version
+    //ocl::HOGDescriptor gpu_hog;
  
     //namedWindow("video capture", CV_WINDOW_NORMAL);
 
     // make a copy of the image
-    Mat img = sourceImg;
+    //UMat img = sourceImg;
     // jkkkkk
     //sourceImg.copyTo(img);
     
-    if (!img.data)
-        return humans;
+    //if (!img.data)
+    //    return humans;
 
     vector<Rect> found;  //, found_filtered;
-    hog.detectMultiScale(img, found, 0, Size(8,8), Size(32,32), 1.05, 2);
+    hog.detectMultiScale(scaledDown, found, 0, Size(8,8), Size(32,32), 1.05, 2);
+    //hog.detectMultiScale(scaledDown, found, 0, Size(8,8), Size(0,0), 1.05, 2);
+    //hog.detectMultiScale(scaledDown, found, hit_threshold, win_stride, Size(0, 0), scaleHog, gr_threshold); // after win_stride is padding
 
+    if(found.size() > 0){
+        //cout << found.size() << " humans found" << endl;
+    }
+    
     size_t i, j;
     for (i=0; i<found.size(); i++)
     {
@@ -40,15 +82,19 @@ std::vector<Human> PersonDetector::detectHumans(Mat sourceImg, bool drawBoxes)
         {
             // Make human
             r.x += cvRound(r.width*0.1);
+            r.x *= 2;
             r.width = cvRound(r.width*0.8);
+            r.width *= 2;
             r.y += cvRound(r.height*0.06);
+            r.y *= 2;
             r.height = cvRound(r.height*0.9);
+            r.height *= 2;
             if(drawBoxes) {
-                rectangle(img, r.tl(), r.br(), cv::Scalar(0,0,0), 2);
+                rectangle(sourceImg, r.tl(), r.br(), cv::Scalar(0,255,0), 3);//cv::Scalar(0,0,0), 2);
             }
             pair<int,int> topleft = make_pair(r.tl().x, r.tl().y);
             pair<int,int> bottomRight = make_pair(r.br().x, r.br().y);
-            Mat humImg(img, r);
+            UMat humImg(sourceImg, r);
             Human nh(std::to_string(Human::idNumber), topleft, bottomRight, humImg);
             Human::idNumber++;
             humans.push_back(nh);
---
+++ sass/src/persondetector.h
@@ -0,0 +1,23 @@
+#ifndef PERSONDETECTOR_H
+#define PERSONDETECTOR_H
+
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include "human.h"
+
+class PersonDetector
+{
+    int runIndependently(); // run independently 
+    
+    public:
+    PersonDetector();
+            
+    // Create humans from the locations of detected humans, 
+    // and optionally draw generic black boxes around the humans
+    std::vector<Human> detectHumans(cv::UMat sourceImg, bool drawBoxes);
+    
+    /* Match new humans with the current stored humans */
+    void keepSimilarHumans(std::vector<Human> & collection, std::vector<Human> & newHumans);
+};
+
+#endif
