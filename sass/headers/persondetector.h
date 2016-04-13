1e60a999e47bbe4a913a8ec09c10e4e2c399401e
Carlton Semple <CarltonSemple@users.noreply.github.com>
2016-04-12 23:21:01 -0400

    UMat for PersonDector

--- sass/headers/persondetector.h
+++ sass/headers/persondetector.h
@@ -14,7 +14,7 @@ class PersonDetector
             
     // Create humans from the locations of detected humans, 
     // and optionally draw generic black boxes around the humans
-    std::vector<Human> detectHumans(cv::Mat sourceImg, bool drawBoxes);
+    std::vector<Human> detectHumans(cv::UMat sourceImg, bool drawBoxes);
     
     /* Match new humans with the current stored humans */
     void keepSimilarHumans(std::vector<Human> & collection, std::vector<Human> & newHumans);
