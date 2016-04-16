c0122171252d7a54d7b91a61a11509e4d6ea51a4
CarltonSemple <csemp@outlook.com>
2016-04-16 04:56:56 -0400

    Drone Decision Making for turning left or right

--- sass/headers/drone.h
+++ sass/headers/drone.h
@@ -1,7 +1,9 @@
 #ifndef DRONE_H
 #define DRONE_H
 
+#include <functional>   // std::ref
 #include <iostream>
+#include <vector>
 #include "opencv2/core.hpp"
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgcodecs.hpp"
@@ -9,24 +11,36 @@
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/calib3d/calib3d.hpp>
 
-class Drone {
+#include "communicationBox.h"
+#include "matcher.h"
+
+class Drone 
+{
     cv::UMat frame;
+    std::vector<cv::UMat> templateImages;
     int windowHeight;
     int windowWidth;
+    int idNum;
+    
     // percentage of distance from the window's sides that is acceptable
     // used to decide if the target is heading out of the frame
     double positionAcceptancePercentage; 
     
+    CommunicationBox & commBox;
+    
 public:
     
-    Drone();
-    void turnLeft(int16_t angle);
-    void turnRight(int16_t angle);
+    Drone(int ID, CommunicationBox & communicationBox);
+    void turnLeft(int angle);
+    void turnRight(int angle);
     
     void decide();
+    void setID(int idnum);
+    
 
 private:
-    void reactToTargetPosition();
+    void reactToTargetPosition(int x, int y);
+    cv::Point getTargetPositionAverage();
 };
 
 #endif
--- sass/src/drone.cpp
+++ sass/src/drone.cpp
@@ -1,16 +1,58 @@
 #include "drone.h"
 
-Drone::Drone()
+using namespace std;
+using namespace cv;
+
+Drone::Drone(int ID, CommunicationBox & communicationBox)
+: commBox(communicationBox) 
 {
     windowWidth = 640;
     windowHeight = 480;
     positionAcceptancePercentage = 0.3;
+    
 }
 
+void Drone::turnLeft(int angle)
+{
+    
+}
 
+void Drone::turnRight(int angle)
+{
+    
+}
 
+void Drone::decide()
+{
+    switch(idNum)
+    {
+    case 1: commBox.drone1Frame.copyTo(frame);
+        break;
+    case 2: commBox.drone2Frame.copyTo(frame);
+        break;
+    }
+    
+    // Get the average target position, in terms of x & y for now
+    Point position = getTargetPositionAverage();
+        
+    reactToTargetPosition(position.x, position.y);    
+}
 
-
+Point Drone::getTargetPositionAverage()
+{
+    double xSum = 0, ySum = 0;
+    
+    Matcher matcher;
+    for(UMat temImg : templateImages)
+    {
+        Point p = matcher.templateMatchingWithoutCallBack(std::ref(temImg), std::ref(frame));
+        xSum += p.x;
+        ySum += p.y;
+    }
+    
+    Point rpoint(xSum/templateImages.size(), ySum/templateImages.size());
+    return rpoint;
+}
 
 // turn in small increments, or move forward until the target is in the center range
 void Drone::reactToTargetPosition(int x, int y)
@@ -18,13 +60,16 @@ void Drone::reactToTargetPosition(int x, int y)
     double centerLeft, centerRight;
     centerLeft = windowWidth * positionAcceptancePercentage;
     centerRight = (windowWidth * positionAcceptancePercentage) - windowWidth;
+    
+    int incAmount = 1;
+    
     // target is left of center range
     if(x < centerLeft) 
     {
-        turnRight(1);
+        turnRight(incAmount);
     }
     else if (x > centerRight) 
     {
-        turnLeft(1);
+        turnLeft(incAmount);
     }
 }
